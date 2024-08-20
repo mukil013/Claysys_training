aab9c6a54a21558f03f9dd6a514ca7c87ee961a7
mukil013 <mukilmukil1947@gmail.com>
2024-08-20 13:32:56 +0530

    update

--- day4&day5/js/app.js
+++ day4&day5/js/app.js
@@ -1,31 +1,74 @@
 let profilePopUp = document.querySelector(".profilePopUp");
 let profileBtn = document.querySelector(".profilePic");
 
-let notificationPopUp = document.querySelector('.notificationPopUp')
-let notification = document.querySelector('.notification')
+let notificationPopUp = document.querySelector(".notificationPopUp");
+let notification = document.querySelector(".notification");
 
-let searchBar = document.querySelector('.searchBar input[type=text]')
-let searchDropDown = document.querySelector('.dropdownItems')
+let searchBar = document.querySelector(".searchBar input[type=text]");
+let searchDropDown = document.querySelector(".dropdownItems");
 
+let bigTableExpand = document.querySelector("#expander");
+let miniTable = document.querySelector(".miniTableParent");
 
-profileBtn.addEventListener('focus', () => {
-  profilePopUp.style.display = "block"
-})
-profileBtn.addEventListener('blur', () => {
-  profilePopUp.style.display = "none"
-})
+let flag = 0,
+  flag1 = 0,
+  hamFlag = 0;
 
-notification.addEventListener('focus', () => {
-    notificationPopUp.style.display = "block"
-})
-notification.addEventListener('blur', () => {
-  notificationPopUp.style.display = "none"
-})
+let hamMenu = document.querySelector("#ham");
+let nav = document.querySelector("aside nav ul");
 
+let pickUpBtn = document.querySelector("#pickUpBtn");
+let pickUpPopUp = document.querySelector(".pickUpPopUp");
 
-searchBar.addEventListener('focus', ()=>{
-  searchDropDown.style.display = 'block'
-})
-searchBar.addEventListener('blur', ()=>{
-  searchDropDown.style.display = 'none'
-})
+let content = document.querySelector(".content");
+
+profileBtn.addEventListener("focus", () => {
+  profilePopUp.style.display = "block";
+});
+profileBtn.addEventListener("blur", () => {
+  profilePopUp.style.display = "none";
+});
+
+notification.addEventListener("focus", () => {
+  notificationPopUp.style.display = "block";
+});
+notification.addEventListener("blur", () => {
+  notificationPopUp.style.display = "none";
+});
+
+searchBar.addEventListener("focus", () => {
+  searchDropDown.style.display = "block";
+});
+searchBar.addEventListener("blur", () => {
+  searchDropDown.style.display = "none";
+});
+
+bigTableExpand.addEventListener("click", () => {
+  if (flag === 0) {
+    miniTable.style.display = "block";
+    flag = 1;
+  } else {
+    miniTable.style.display = "none";
+    flag = 0;
+  }
+});
+
+pickUpBtn.addEventListener("click", () => {
+  if (flag === 0) {
+    pickUpPopUp.style.display = "block";
+    flag = 1;
+  } else {
+    pickUpPopUp.style.display = "none";
+    flag = 0;
+  }
+});
+
+hamMenu.addEventListener("click", () => {
+  if (hamFlag == 0) {
+    nav.style.display = "block";
+    hamFlag = 1;
+  } else {
+    nav.style.display = "none";
+    hamFlag = 0;
+  }
+});
