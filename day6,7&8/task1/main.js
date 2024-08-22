2363156ec0c41c226ed60ebaa75f1a835235f3b2
mukil013 <mukilmukil1947@gmail.com>
2024-08-22 17:39:41 +0530

    task 1

--- day6,7&8/task1/main.js
+++ day6,7&8/task1/main.js
@@ -1,21 +1,39 @@
-let calculator = document.querySelector("body form");
+let calculator = document.querySelector("form");
 let interest = document.querySelector(".interest");
 let total = document.querySelector(".total")
 let addInfo = document.querySelector(".add-info")
 let res = 0;
 
 calculator.addEventListener("submit", (e) => {
-  let p = document.querySelector("#p").value
-  let n = document.querySelector("#n").value
-  let r = document.querySelector("#r").value
+
+  let p = Number(document.querySelector("#p").value)
+  let n = Number(document.querySelector("#n").value)
+  let r = Number(document.querySelector("#r").value)
+
   e.preventDefault();
+
   if(p < 500 || p > 10000){
+
     alert("Enter principal amount from $500 to $10000")
     calculator.reset()
+
   }else{
-    res = (p*n*r)/100
+
+    let addedBonus = 0
+
+    if(n > 5){
+      let temp = r + 2
+      addedBonus = (p / 100) * temp 
+    }
+
+    else if( p < 1000 && r < 5) r = 5
+    else if((p >= 1000 || p < 5000 ) && n < 7) n = 7
+    else if( p > 5000 && n < 10) n = 10
+
+    res = Number((p*n*r)/100)
     interest.textContent = res
-    total.textContent = Number(p)+Number(res)
-    addInfo.textContent = 0
+    total.textContent = res+p
+    addInfo.textContent = addedBonus
+    
   }
 });
